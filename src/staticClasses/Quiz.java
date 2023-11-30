83979af758fa6c4c6e8a881270643d3ac4cc7b96
Ruairí Glackin <ruairiglackin@gmail.com>
2023-11-30 17:39:26 +0000

    ..

--- src/staticClasses/Quiz.java
+++ src/staticClasses/Quiz.java
@@ -29,16 +29,6 @@ public class Quiz {
         this.u = u;
     }
 
-    /*public int getQuizType() {
-        return quizType;
-    }
-
-    public void setQuizType(int quizType) {
-        if (quizType >= 0 && quizType <= 2) {
-            this.quizType = quizType;
-        }
-    }*/
-
     public int getMark() {
         return mark;
     }
@@ -55,78 +45,14 @@ public class Quiz {
         this.time = time;
     }
 
-    
-
-    public Quiz() {
-        
-    }
-
     public void writeResult() {
-        /*String pathnameString = "";
-        switch (type) {
-            case INCREASING_DIFFICULTY:
-                pathnameString = "incDiffResults.txt";
-                break;
-            case RANDOM:
-                pathnameString = "randResults.txt";
-                break;
-            case TIMED:
-                writeTimerResult();
-                return;
-        }
-        try {
-            File myObj = new File(pathnameString);
-            myObj.createNewFile();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(pathnameString, true));
-
-            Scanner reader = new Scanner(pathnameString);
-            while (reader.hasNextLine()) {
-                // writer.write("\n");
-                reader.nextLine();
-
-            }
-            reader.close();
-
-            writer.write("\n"+u.getID() + " " + getType().toString() + " " + u.getUsername() + " " + this.getMark()+"\n");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        
         
         String query = String.format("INSERT INTO quiz_result (score,quiz_type,user_id,timer) VAlUES (%s,'%s',%s,%s)",mark,type.toString(),u.getID(),time);
         DAL.InsertQuery(query);
 
     }
-    /*public void writeTimerResult(){
-        try {
-            File myObj = new File("timerResults.txt");
-            myObj.createNewFile();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("timerResults.txt", true));
-
-            Scanner reader = new Scanner("timerResults.txt");
-            while (reader.hasNextLine()) {
-                // writer.write("\n");
-                reader.nextLine();
-
-            }
-            reader.close();
-
-            //writer.write(u.getID() + " " + this.getQuizType() + " " + u.getUsername() + " " + this.getMark() + " " +this.getTime()+"\n");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
+    
 
     
 
