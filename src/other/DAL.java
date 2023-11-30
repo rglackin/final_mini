125711780ec369c8b0a07b76b8fe4267ff6f8965
Ruairí Glackin <ruairiglackin@gmail.com>
2023-11-30 00:29:36 +0000

    db

--- src/other/DAL.java
+++ src/other/DAL.java
@@ -2,27 +2,22 @@ package other;
 import java.sql.*;
 
 
+
 public class DAL {
-    static final String connectionString = "jdbc:sqlite:epic_db.db";
-    
+    static final String connectionString = "jdbc:mysql://localhost:3306/";
+    static final String USER = "root";
+    static final String PASS = "password";
     public static void Test() {
-        Class.forName("org.sqlite.JDBC");
-        try (Connection connection = DriverManager.getConnection(connectionString);
-                Statement statement = connection.createStatement()) {
-                    PreparedStatement selectStatement = connection.prepareStatement("select * from users");
-            ResultSet rs = selectStatement.executeQuery();
-
-            //List<User> users = new ArrayList<>();
-
-            while (rs.next()) { // will traverse through all rows
-                Integer id = rs.getInt("id");
-                String username = rs.getString("first_name");
-                String password = rs.getString("last_name");
-                System.out.printf("%s\n%s\n%s",id,username,password);
-            }
-
-        } catch (SQLException e) {
+        try(Connection conn = DriverManager.getConnection(connectionString, USER, PASS);
+        Statement stmt = conn.createStatement();){
+            String sql = "";
+            stmt.executeQuery(sql);
+                
+        }
+        catch(SQLException e){
             e.printStackTrace();
         }
     }
+    
+    
 }
